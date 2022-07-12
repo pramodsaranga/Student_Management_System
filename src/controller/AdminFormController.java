137bceefae7316b542b00ba5735c9fed41a98aec
pramodsaranga <pramodsaranga31@gmail.com>
2022-07-12 13:40:44 +0530

    Updeted context

--- src/controller/AdminFormController.java
+++ src/controller/AdminFormController.java
@@ -1,11 +1,28 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class AdminFormController {
     public AnchorPane AdminFormContext;
+    public AnchorPane StudentContext;
 
-    public void StudentManageFormOnAction(ActionEvent actionEvent) {
+    public void StudentManageFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/StudentManageForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        StudentContext.getChildren().clear();
+        StudentContext.getChildren().add(load);
+    }
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/DashBoardForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        AdminFormContext.getChildren().clear();
+        AdminFormContext.getChildren().add(load);
     }
 }
---
+++ src/controller/StudentManageFormController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentManageFormController {
+    public TableView tblStudent;
+    public TableColumn colStudentID;
+    public TableColumn colStudentName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNIC;
+    public TextField txtStudentID;
+    public TextField txtNIC;
+    public TextField txtAddress;
+    public TextField txtContact;
+    public TextField txtStudentName;
+    public TextField txtEmail;
+
+    public void studentAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void studentUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void studentDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void studentSearchOnAction(ActionEvent actionEvent) {
+    }
+}
